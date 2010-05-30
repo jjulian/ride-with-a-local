fca5b6a6369458924aad28691b73c056504242f3
Jonathan Julian <jonathan.m.julian@gmail.com>
2010-05-30 16:41:27 -0400

    add starting point seed data for the drivers. generate api_key on create

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,9 +1,14 @@
+require 'digest'
 class Taxi < ActiveRecord::Base
-  attr_accessible :name, :handle, :phone, :description
   validates_presence_of :name, :description
   has_many :locations
+  before_create { |taxi| taxi.generate_api_key if taxi.api_key.blank? }
   
   def last_known_location
     locations.last
+  end
+  
+  def generate_api_key
+    self.api_key = Digest.hexencode(self.name + self.description)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+
+Taxi.create!([
+  { :name => 'Jonathan', :license => nil, :phone => '4438031427', :photo_url => '/images/jonathan-mini.jpg', 
+    :description => 'Blue and black MINI Cooper S. I\'ll get you downtown fast, but I can only take one normal-sized and one small person.' },
+  { :name => 'Nick', :license => nil, :phone => nil, :photo_url => '/images/ngauthier-car.jpg', 
+    :description => 'temp' },
+  { :name => 'Yair', :license => nil, :phone => nil, :photo_url => nil, 
+    :description => 'temp' },
+  { :name => 'Richard', :license => nil, :phone => nil, :photo_url => nil, 
+    :description => 'temp' },
+  { :name => 'Josh', :license => nil, :phone => nil, :photo_url => nil, 
+    :description => 'temp' },
+  { :name => 'Gordon', :license => nil, :phone => nil, :photo_url => nil, 
+    :description => 'temp' }
+])
