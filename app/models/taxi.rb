705f62548a0b3ee6e15b355c22a03705de6495ab
Jonathan Julian <jonathan.m.julian@gmail.com>
2010-05-30 20:10:28 -0400

    upon further reflection, static api_keys will survive thru database resets

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,14 +1,8 @@
-require 'digest'
 class Taxi < ActiveRecord::Base
-  validates_presence_of :name, :description
+  validates_presence_of :name, :description, :api_key
   has_many :locations
-  before_create { |taxi| taxi.generate_api_key if taxi.api_key.blank? }
   
   def last_known_location
     locations.last
-  end
-  
-  def generate_api_key
-    self.api_key = Digest.hexencode(self.name + self.description)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+require 'digest'
+
 Taxi.create!([
-  { :name => 'Jonathan', :license => nil, :phone => '4438031427', :photo_url => '/images/jonathan-mini.jpg', 
+  { :name => 'Jonathan', :license => nil, :phone => '4438031427', :photo_url => '/images/jonathan-mini.jpg', :api_key => Digest.hexencode('jonathan'),
     :description => 'Blue and black MINI Cooper S. I\'ll get you downtown fast, but I can only take one normal-sized and one small person.' },
-  { :name => 'Nick', :license => nil, :phone => nil, :photo_url => '/images/ngauthier-car.jpg', 
+  { :name => 'Nick', :license => nil, :phone => nil, :photo_url => '/images/ngauthier-car.jpg', :api_key => Digest.hexencode('nick'),
     :description => 'temp' },
-  { :name => 'Yair', :license => nil, :phone => nil, :photo_url => nil, 
+  { :name => 'Yair', :license => nil, :phone => nil, :photo_url => nil, :api_key => Digest.hexencode('yair'),
     :description => 'temp' },
-  { :name => 'Richard', :license => nil, :phone => nil, :photo_url => nil, 
+  { :name => 'Richard', :license => nil, :phone => nil, :photo_url => nil, :api_key => Digest.hexencode('richard'),
     :description => 'temp' },
-  { :name => 'Josh', :license => nil, :phone => nil, :photo_url => nil, 
+  { :name => 'Josh', :license => nil, :phone => nil, :photo_url => nil, :api_key => Digest.hexencode('josh'),
     :description => 'temp' },
-  { :name => 'Gordon', :license => nil, :phone => nil, :photo_url => nil, 
+  { :name => 'Gordon', :license => nil, :phone => nil, :photo_url => nil, :api_key => Digest.hexencode('gordon'),
     :description => 'temp' }
 ])
